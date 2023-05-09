0d0ae84f703286fd71061633e5507f84bbaf63b3
JuanYanqui <juan.yanqui.est@tecazuay.edu.ec>
2023-05-08 22:48:06 -0500

    Guardar solicitud convocatoria
    Guardar solicitud convocatoria

--- src/main/java/com/ExamenComplexivo/ProyectoPracticas/models/dao/primary/global/IEstudiantePracticanteDao.java
+++ src/main/java/com/ExamenComplexivo/ProyectoPracticas/models/dao/primary/global/IEstudiantePracticanteDao.java
@@ -2,6 +2,15 @@ package com.ExamenComplexivo.ProyectoPracticas.models.dao.primary.global;
 
 import com.ExamenComplexivo.ProyectoPracticas.models.entity.primary.Estudiante_Practicante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IEstudiantePracticanteDao extends JpaRepository<Estudiante_Practicante,Long> {
+    @Query("select es from Estudiante_Practicante es join es.usuario_estudiante_practicante us where us.cedula = :cedula")
+    List<Estudiante_Practicante> findByCedula(@Param("cedula") String cedula);
+
+    @Query("SELECT e.idEstudiantePracticas FROM Estudiante_Practicante e JOIN e.usuario_estudiante_practicante us WHERE us.cedula = :cedula")
+    Long findIdByCedula(@Param("cedula") String cedula);
 }
--- src/main/java/com/ExamenComplexivo/ProyectoPracticas/models/services/primary/global/services/IEstudiantePracticanteService.java
+++ src/main/java/com/ExamenComplexivo/ProyectoPracticas/models/services/primary/global/services/IEstudiantePracticanteService.java
@@ -3,5 +3,10 @@ package com.ExamenComplexivo.ProyectoPracticas.models.services.primary.global.se
 import com.ExamenComplexivo.ProyectoPracticas.models.entity.primary.Estudiante_Practicante;
 import com.ExamenComplexivo.ProyectoPracticas.models.services.primary.generic.IGenericService;
 
+import java.util.List;
+
 public interface IEstudiantePracticanteService extends IGenericService<Estudiante_Practicante,Long> {
+    List<Estudiante_Practicante> findByCedula(String cedula);
+
+    Long findIdByCedula(String cedula);
 }
